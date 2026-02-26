ef03629dccbe0ba443eab8c9bb153fbf6b7164bf
rhiguchi-san <rhiguchi1203@gmail.com>
2026-02-27 00:00:29 +0900

    kadai_018を追記

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -1,5 +1,22 @@
 package kadai.kadai_018;
 
-public class Kato_Chapter18 {
+abstract public class Kato_Chapter18 {
+	public String familyName = "加藤";
+	public String givenName = "";
+	public String address = "東京都中野区〇×";
 
+	//共通の紹介を出力する
+	public void commonIntroduce() {
+		System.out.println("住所は" + address + "です");
+	}
+
+	//個々の紹介を出力する
+	abstract public void eachIntroduce();
+
+	//紹介を実行する
+	public void execIntroduce() {
+		System.out.println("名前は" + familyName + givenName + "です");
+		commonIntroduce();
+		eachIntroduce();
+	}
 }
